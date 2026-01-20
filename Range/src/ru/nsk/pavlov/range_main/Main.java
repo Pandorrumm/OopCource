00d1cfd97ffd3fafb4914f4fb7a57d51851f5a11
VitalyPavlov <fankit8686@gmail.com>
2026-01-20 09:21:17 +0700

    Create Range and examples

--- Range/src/ru/nsk/pavlov/range_main/Main.java
+++ Range/src/ru/nsk/pavlov/range_main/Main.java
@@ -1,7 +1,48 @@
 package ru.nsk.pavlov.range_main;
 
+import ru.nsk.pavlov.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the beginning number of the range:");
+        double from = scanner.nextDouble();
+
+        System.out.println("Enter the ending number of the range:");
+        double to = scanner.nextDouble();
+
+        System.out.println("Enter the number:");
+        double number = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+
+        System.out.printf("Range length: %.3f%n", range.getLength());
+
+        printIsInsideNumberInfo(range, number);
+
+        System.out.println("Enter the new beginning number of the range:");
+        double newFrom = scanner.nextDouble();
+
+        System.out.println("Enter the new ending number of the range:");
+        double newTo = scanner.nextDouble();
+
+        range.setFrom(newFrom);
+        range.setTo(newTo);
+
+        System.out.println("The range has been redefined!");
+        System.out.printf("New range length: %.3f%n", range.getLength());
+
+        printIsInsideNumberInfo(range, number);
+    }
+
+    public static void printIsInsideNumberInfo(Range range, double number) {
+        if (range.isInside(number)) {
+            System.out.println("Your number is in the range from " + range.getFrom() + " to " + range.getTo());
+        } else {
+            System.out.println("Your number is out of range from " + range.getFrom() + " to " + range.getTo());
+        }
     }
 }
